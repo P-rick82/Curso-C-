4fee8d78d2435842df652f57d5441f50361ec726
P-rick82 <patrikaugustor800@gmail.com>
2026-04-28 17:05:21 -0300

    Aula 28-04 conteudos e exercicios

---
+++ Aula 20-04/Conteudo da aula/Program.cs
@@ -0,0 +1,175 @@
+﻿// namespace Curso C#;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Console.WriteLine("Digite um numero");
+        // int n = int.Parse(Console.ReadLine());
+
+        // if(n > 0)
+        // {
+        //     Console.WriteLine("Positivo");
+        // }
+        // else if(n == 0)
+        // {
+        //     Console.WriteLine("O numero é zero");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Negativo");
+        // }
+
+        // ____________________________________________________
+
+        // Console.Write("Digite sua idade: ");
+        // int idade = int.Parse(Console.ReadLine());
+
+        // if(idade < 16)
+        // {
+        //     Console.WriteLine("Não pode votar");
+        // }
+        // else if(idade < 18)
+        // {
+        //     Console.WriteLine("Voto opicional");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Voto Obrigatorio");
+        // }
+
+        // _____________________________________________________
+
+        // Console.Write("Digite a nota: ");
+        // double nota = double.Parse(Console.ReadLine());
+
+        // if(nota >= 7)
+        // {
+        //     Console.WriteLine("Aprovado!");
+        // }
+        // if(nota >= 9)
+        // {
+        //     Console.WriteLine("Aprovado e ganhou estrelinha!");
+        // }
+        // else if(nota >= 5)
+        // {
+        //     Console.WriteLine("Recuperação");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Recuprovado");
+        // }
+
+        // _____________________________________________________________
+
+        // int n1 = 25;
+        // int n2 = 5;
+
+        // if(n1 > n2)
+        // {
+        //     Console.WriteLine("Primeiro é maior");
+        // }
+        // else if(n2 > n1)
+        // {
+        //     Console.WriteLine("Segundo é maior");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Numeros são iguais");
+        // }
+
+        // __________________________________________________________
+
+        // Console.WriteLine("Usuario: ");
+        // string Usuario = Console.ReadLine();
+
+        // Console.WriteLine("Senha: ");
+        // string Senha = Console.ReadLine();
+
+        // if(Usuario == "admin" && Senha == "1234")
+        // {
+        //     Console.WriteLine("Acesso permitido");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("Acesso negado");
+        // }
+
+        // __________________________________________________________
+
+        // Console.WriteLine("Digite um numero");
+        // int n = int.Parse(Console.ReadLine());
+
+        // // if ternario
+        // string resultado = n % 2 == 0 ? "É par" : "É impar";
+        // Console.WriteLine(resultado);
+
+        // Outro modo
+
+        // if(n % 2 == 0)
+        // {
+        //     Console.WriteLine("É par");
+        // }
+        // else
+        // {
+        //     Console.WriteLine("É impar");
+        // }
+
+        // _____________________________________________________________
+
+        // int idade = 22;
+
+        // Console.WriteLine(idade >= 18 ? "Maior de idade" : "Menor de idade");
+
+        // ______________________________________________________________________
+
+        //Switch case
+        // Console.Write("Digite um numero entre 1 e 3: ");
+        // int opcao = int.Parse(Console.ReadLine());
+
+        // switch(opcao)
+        // {
+            // case 1:
+            //  Console.WriteLine("Vc escolheu a opção 1.");
+            // break;
+
+            // case 2:
+            // Console.WriteLine("Vc escolheu a opção 2.");
+            // break;
+
+            // case 3:
+            // Console.WriteLine("Vc escolheu a opção 3");
+            // break;
+            
+            // default:
+            // Console.WriteLine("Opção invalida!");
+            // break;
+        // }
+
+        // __________________________________________________________________
+
+        //Switch case (com .ToUpper() (função no codigo permite q a resposta em string possa ser tanto em maiusculo quanto em minusculo, impedindo problemas na hora de rodar condicionais))
+//         Console.Write("Digite uma opção: (A, B ou C): ");
+//         string opcao = Console.ReadLine().ToUpper();
+
+//         switch(opcao)
+//         {
+//             case "A":
+//                 Console.WriteLine("Vc escolheu a opção A.");
+//                 break;
+
+//             case "B":
+//                 Console.WriteLine("Vc escolheu a opção B.");
+//                 break;
+
+//             case "C":
+//                 Console.WriteLine("Vc escolheu a opção C.");
+//                 break;
+            
+//             default:
+//                 Console.WriteLine("Opção invalida!");
+//                 break;
+//         }
+
+    }
+}
