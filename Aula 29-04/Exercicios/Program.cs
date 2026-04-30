1c49b8430ef64844dfe63e6135b842970d825920
P-rick82 <patrikaugustor800@gmail.com>
2026-04-30 17:21:17 -0300

    Novos conteúdos

--- Aula 29-04/Exercicios/Program.cs
+++ Aula 29-04/Exercicios/Program.cs
@@ -4,6 +4,102 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       
+    //Exercicio 1 - Contagem crescente (for): Mostre na tela os números de 1 até 10.
+
+    //    for(int i = 1; i <=10; i++)
+    //    {
+    //     Console.WriteLine(i);
+    //    }
+
+    //Exercicio 2 - Contagem regressiva (for): Mostre na tela os números de 10 até 1.
+
+    //    for(int i = 10; i >=1; i--)
+    //    {
+    //     Console.WriteLine(i);
+    //    }
+
+    //Exercicio 3: Tabuada simples (for): Leia um número e mostre a tabuada dele de 1 a 10.
+       
+    // Console.Write("Digite um numero para obter sua Tabuada: ");
+    // int n = int.Parse(Console.ReadLine());
+
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     Console.WriteLine(i*n);
+    // }
+
+    //Exercicio 4: Repetição com while: Mostre os números de 1 até 5 usando while.
+
+    // int n = 1;
+
+    //     while (n <= 5)
+    //     {
+    //         Console.WriteLine(n);
+    //         n++;
+    //     }
+
+    //Exercicio 5: Soma até 5 (for): Some os números de 1 até 5 e mostre o resultado.
+
+    // int soma = 0;
+
+    //     for (int i = 1; i <= 5; i++)
+    //     {
+    //         soma += i;
+    //     }
+
+    //     Console.WriteLine("Soma: " + soma);
+
+    //Exercicio 5: Soma de números digitados (for): Peça 5 números ao usuário e mostre a soma total.
+
+    // Console.Write("Digite um numero: ");
+    // int n1 = int.Parse(Console.ReadLine());
+
+    // Console.Write("Digite um novo numero: ");
+    // int n2 = int.Parse(Console.ReadLine());
+
+    // Console.Write("Digite um novo numero: ");
+    // int n3 = int.Parse(Console.ReadLine());
+
+    // Console.Write("Digite um novo numero: ");
+    // int n4 = int.Parse(Console.ReadLine());
+
+    // Console.Write("Digite um novo numero: ");
+    // int n5 = int.Parse(Console.ReadLine());
+        
+    // int soma = 0;
+
+    // for(int i = n1; i <= n5; i++)
+    // {
+    //     soma += i;
+    // }
+
+    // Console.WriteLine("Soma: " + soma);
+
+    //Codigo reduzido:
+
+        int limite = 5; 
+        
+        double[] numeros = new double[limite]; 
+        int contador = 0;
+
+        Console.WriteLine($"Digite {limite} números.");
+
+        while (contador < limite)
+        {
+            Console.Write($"Digite o {contador + 1}º número: ");
+            numeros[contador] = double.Parse(Console.ReadLine());
+            contador++;
+        }
+
+        double soma = 0;
+        foreach (double n in numeros) 
+        {
+            soma += n;
+        }
+
+        Console.WriteLine($"\nA soma dos {limite} números é: {soma}");
+
+
     }
 }
---
+++ Aula 30-04/Program.cs
@@ -0,0 +1,143 @@
+﻿namespace Aula_30_04;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Funções
+
+        /*
+        Tipos_de_retorno Nome_da_Função(Parametros) ou Tiposderetorno NomedaFunção(Parametros)
+        {
+            Comandos da Função
+        }
+        */
+
+        //Exemplo:
+
+        // //Função sem parametros e sem retorno
+        
+        // static void Mostrar_Mensagem()
+        // {
+        //     Console.WriteLine("Hello World");
+        // }
+
+        // //Chama a função
+        // Mostrar_Mensagem();
+
+       // // Função com Parametro
+        
+        // static void Saudacao(string nome)
+        // {
+        //     Console.WriteLine($"Hi, {nome}!");
+        // }
+
+        // Saudacao("lek");
+
+        //Função com retorno
+        
+        //Parametros a e b
+        //Retorno é a soma
+
+        // static int somar(int a, int b)
+        // {
+        //     // int soma = a + b;  é msm coisa q a linha abaixo
+        //     return a + b;
+        // }
+
+        // int resultado = somar(15,30);
+        // Console.WriteLine(resultado);
+
+        //Outro exemplo
+
+        // static void exibirdobro(int num)
+        // {
+        //     Console.WriteLine(num*2);
+        // }
+        // exibirdobro(10);
+
+        //Arrays -> Armazena varios valores do mesmo tipo em uma unica variavel
+
+        // int[] numeros = new int[5];
+
+        // Console.WriteLine(numeros);
+
+        // numeros[0] = 2;
+        // numeros[1] = 5;
+        // numeros[2] = 8;
+        // numeros[3] = 15;
+        // numeros[4] = 20;
+
+        // Console.WriteLine(numeros);
+        // Console.WriteLine(string.Join(",", numeros));
+
+        // //Outro exemplo
+
+        // string[] nomes = {"Zezé di Camargo", "Amado Batista", "Leonardo"};
+        
+        // //Percorrendo com for
+
+        // for (int i = 0; i < nomes.Length; i++)
+        // {
+        //     Console.WriteLine(nomes[i]);
+        // }
+
+        // //Percorrendo com foreach
+
+        // foreach(string nome in nomes)
+        // {
+        //     Console.WriteLine(nume);
+        // }
+
+        // // Função + Array
+        
+        // static double CalcularMedia(double[] notas)
+        // {
+        //     double soma = 0;
+
+        //     for(int i =0; i < notas.Length; i++)
+        //     {
+        //         //soma = soma + nota[posição]
+        //         soma += notas[i];
+        //     }
+
+        //     return soma / notas.Length;
+        // }
+
+        // double[] notasAluno = {8.0, 7.5, 9.0, 6.5};
+        
+        // double media = CalcularMedia(notasAluno);
+
+        // Console.WriteLine($"Média final: {media}");
+
+        //Outro exemplo
+
+        // int[] numeros = new int[5];
+        // int soma = 0;
+
+        // for(int i = 0; i < numeros.Length; i++)
+        // {
+        //     Console.WriteLine($"Digite o {i + 1}º número: ");
+        //     numeros[i] = int.Parse(Console.ReadLine());
+
+        //     soma += numeros[i];
+            
+        // }
+
+        // Console.WriteLine(string.Join(",", numeros));
+        // Console.WriteLine($"A soma dos números é: {soma}");
+
+        //Exercicio Realizado durante a aula
+
+        static string VerificarSituação(double nota)
+        {
+            if(nota >= 7);
+            {
+                return "Aprovado";
+
+            }
+        }        
+
+
+    }
+}
